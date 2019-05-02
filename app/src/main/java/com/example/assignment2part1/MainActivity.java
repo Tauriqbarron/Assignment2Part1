73c65b59d4041bd3c8da6c2bf37397039fc08a7a
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-05-02 15:09:01 +1200

    updated MainAct. Linked all Layout widgets to Mainact and set up btn search on click listener. Added search button to layout xml.

--- app/src/main/java/com/example/assignment2part1/MainActivity.java
+++ app/src/main/java/com/example/assignment2part1/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.assignment2part1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 // www.wordsapi.com
+    TextView searchText;
+    TextView searchResult;
+    Button btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchText = findViewById(R.id.wordSearchTxt);
+        searchResult = findViewById(R.id.definitionTxt);
+        btnSearch = findViewById(R.id.btnSearch);
 
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
 }
