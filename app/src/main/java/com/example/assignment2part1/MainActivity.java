402c62bb7522e2628f8dc758b1fa8a5f53fdabea
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-05-07 18:47:14 +1200

    updated MainAct. Linked all Layout widgets to Mainact and set up btn search on click listener. Added search button to layout xml.

--- app/src/main/java/com/example/assignment2part1/MainActivity.java
+++ app/src/main/java/com/example/assignment2part1/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.assignment2part1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Header;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpResponse;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 // www.wordsapi.com
+
+
+
     TextView searchText;
     TextView searchResult;
     Button btnSearch;
@@ -15,14 +32,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         searchText = findViewById(R.id.wordSearchTxt);
         searchResult = findViewById(R.id.definitionTxt);
         btnSearch = findViewById(R.id.btnSearch);
 
+
+        final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                CharSequence wordSearch =searchText.getText();
+
 
             }
         });
