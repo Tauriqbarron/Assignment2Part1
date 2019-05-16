bd55922ee882b9e707ea0099c869b499d73b9c35
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-05-16 20:52:53 +1200

    Added Josn api Jar module, and set up String to Json object

--- app/src/main/java/com/example/assignment2part1/MainActivity.java
+++ app/src/main/java/com/example/assignment2part1/MainActivity.java
@@ -23,8 +23,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import org.json.*;
+
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonString;
 
 public class MainActivity extends AppCompatActivity {
 // www.wordsapi.com
@@ -57,22 +65,38 @@ public class MainActivity extends AppCompatActivity {
                 StringRequest stRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        StringBuilder txtResult = new StringBuilder();
                         String resp = response;
-                        searchResult.setText(resp.toString());
+                        try {
+                            JsonObject res = JS
+                            JSONArray defArray = res.getJSONArray("definitions");
+
+                            for(int idx = 0; idx< defArray.length(); idx++){
+                                JSONObject curDef = defArray.getJSONObject(idx);
+                                JSONObject definition = curDef.getJSONObject("definition");
+                                txtResult.append(definition.getString("definition")+"/n");
+                            }
+                        }catch (Throwable t){
+                            Log.e("Error","Could not parse");
+                        }
+
+                        //searchResult.setText(resp.toString());
                         System.err.println(response);
+
+
                     }
 
 
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("error",error.getLocalizedMessage());
+                        Log.e("error",error.toString());
                     }
                 }){
                     @Override
                     public Map<String,String> getHeaders() throws AuthFailureError{
                         Map<String,String> params = new HashMap<>();
-                        params.put("X-Mashape-Key","TO DO: COPY KEY ");
+                        params.put("X-Mashape-Key","dd1d560155msh2a3a50aa39ffde2p13e582jsne6443e91eb58");
                         params.put("Accept","text/plain");
                         return params;
                     }
@@ -80,5 +104,13 @@ public class MainActivity extends AppCompatActivity {
                 queue.add(stRequest);
             }
         });
+    }
+    private static JsonObject jsonFromString(String jsonObjectStr) {
+
+        JsonReader jsonReader = Json.createReader(new StringReader(jsonObjectStr));
+        JsonObject object = jsonReader.readObject();
+        jsonReader.close();
+
+        return object;
     }
 }
