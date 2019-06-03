5fe3443d54f91e0f1170b0e1e893dce5c97081e9
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-06-03 13:17:37 +1200

    Added Comments

--- app/src/main/java/com/example/assignment2part1/MainActivity.java
+++ app/src/main/java/com/example/assignment2part1/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.assignment2part1;
-
 import android.net.Uri;
+import android.support.multidex.MultiDex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Header;
 import com.android.volley.Request;
@@ -18,45 +17,37 @@ import com.android.volley.toolbox.HttpResponse;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonString;
-
 public class MainActivity extends AppCompatActivity {
 // www.wordsapi.com
-
-
-
     TextView searchText;
     TextView searchResult;
     Button btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MultiDex.install(getBaseContext());
         setContentView(R.layout.activity_main);
         searchText = findViewById(R.id.wordSearchTxt);
         searchResult = findViewById(R.id.definitionTxt);
         btnSearch = findViewById(R.id.btnSearch);
-
-
         final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
 
-
+        // This method takes what ever word is in the text field send it as a string request to the
+        // wordsapi.com API and returns all definitions that match that word.
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CharSequence wordSearch =searchText.getText();
-
                 String url = Uri.parse("https://wordsapiv1.p.mashape.com/words/"+wordSearch+
                         "/definitions").buildUpon().toString();
-
                 StringRequest stRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -74,14 +65,8 @@ public class MainActivity extends AppCompatActivity {
                         } catch (Throwable t){
                           Log.e("Error","Could not parse");
                        }
-
-                        //searchResult.setText(resp.toString());
                         System.err.println(response);
-
-
                     }
-
-
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
@@ -100,12 +85,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    // This function takes the in the string response and converts it into a Json object then returns
+    // the Json object.
     private static JsonObject jsonFromString(String jsonObjectStr) {
-
         JsonReader jsonReader = Json.createReader(new StringReader(jsonObjectStr));
         JsonObject object = jsonReader.readObject();
         jsonReader.close();
-
         return object;
     }
 }
